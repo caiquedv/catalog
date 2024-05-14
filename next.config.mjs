5098c35d7363b4edde06b90eef9aa87416b22489
Caique <caique.log@gmail.com>
2024-05-13 22:02:02 -0300

    Remove uso do modules do config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/catalog",
   output: "export",
   reactStrictMode: true
-}
- 
-module.exports = nextConfig
+};
+
+export default nextConfig;
