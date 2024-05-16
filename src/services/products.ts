934bc0d3479779b32740e1f07ba88d1953f721dc
Caique <caique.log@gmail.com>
2024-05-15 21:41:32 -0300

    service

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,18 +1,20 @@
 export type ProductType = {
-    id: number
-    name: string
-    description: string
-    price: number
-    imageUrl: string
-    inStock: number
-  }
-  
-  export const fetchProducts = async () => {
-    const products: ProductType[] = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`).then(res => res.json())
-    return products
-  }
-  
-  export const fetchProduct = async (id: string | number) => {
-    const product: ProductType = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products/${id}`).then(res => res.json())
-    return product
-  }
+  id: number
+  name: string
+  description: string
+  price: number
+  imageUrl: string
+  inStock: number
+}
+
+export const fetchProducts = async () => {
+  const apiUrl = 'https://caiquedv.github.io/catalog/api/products';
+  const products: ProductType[] = await fetch(apiUrl).then(res => res.json());
+  return products;
+}
+
+export const fetchProduct = async (id: string | number) => {
+  const apiUrl = `https://caiquedv.github.io/catalog/api/products/${id}`;
+  const product: ProductType = await fetch(apiUrl).then(res => res.json());
+  return product;
+}
