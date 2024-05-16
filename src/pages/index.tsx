b7c0cea0e0060a125d571b5ee61861933aba8d46
Caique <caique.log@gmail.com>
2024-05-15 21:51:57 -0300

    service

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,20 @@
-import { GetStaticProps, NextPage } from 'next'
-import Head from 'next/head'
-import { ReactNode } from 'react'
-import { Container } from 'reactstrap'
-import Header from '../components/Header'
-import ProductsList from '../components/ProductsList'
-import { fetchProducts, ProductType } from '../services/products'
+import { NextPage } from "next";
+import Head from "next/head";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const products = await fetchProducts()
-  return { props: { products } }
-}
-
-const Products: NextPage = (props: {
-  children?: ReactNode
-  products?: ProductType[]
-}) => {
+const Cart: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Nossos Produtos</title>
-        <meta name="description" content="Conheça todos os nossos produtos" />
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho de compras" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
-
-      <main>
-        <Container className="mb-5">
-          <h1 className="my-5">
-            Nossos Produtos
-          </h1>
-
-          {<ProductsList products={props.products!} />}
-        </Container>
-      </main>
+      <h1>
+        Carrinho
+      </h1>
     </>
   )
 }
 
-export default Products
+export default Cart
