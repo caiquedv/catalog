ee6d4e5c0b557611c239db57e2b9325ac0252a54
Caique <caique.log@gmail.com>
2024-05-18 13:09:34 -0300

    Altera numero do whats

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -7,12 +7,9 @@ type ProductCardProps = {
 };
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
-  // Initializing the router
   const router = useRouter();
 
-  // Function to handle the buy button click event
   const handleBuyButtonClick = () => {
-    // Navigating to the purchase page with query parameters
     router.push({
       pathname: '/purchase',
       query: {
@@ -22,18 +19,12 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     });
   };
 
-  // Rendering the product card component
   return (
     <Card>
-      {/* Rendering card content */}
       <CardBody>
-        {/* Rendering product name */}
         <h5 className="card-title">{product.name}</h5>
-        {/* Rendering product image */}
         <img src={product.imageUrl} alt={product.name} className="card-img-top" />
-        {/* Rendering product price */}
         <CardSubtitle className="mb-3 text-muted">R$ {product.price}</CardSubtitle>
-        {/* Rendering buy button */}
         <Button color="dark" className="pb-2" block onClick={handleBuyButtonClick}>
           Comprar
         </Button>
